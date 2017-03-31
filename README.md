93f545eaef8eabde913dec3c5947d9fe0d48bd43
root <flexo01010@gmail.com>
2017-03-31 13:32:29 +1100

    tasty_downloader.py added to tools

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Newly-Registered-Domains-Tools
 Python scripts used to scrape newly registered domains names and search through them
+
+## tasty-downloader.py
+Basic python application for downloading newly registered domains
+
+### Usage
+```
+python tasty_download.py
+```
+
+This creates an output file. An example of the output file is below:
+```
+newdomain1.com
+newdomain2.com
+newdomain3.net
+newdomain4.com
+...
+
---
+++ tasty_downloader.py
@@ -0,0 +1,80 @@
+#~/usr/bin/python
+
+import requests
+import re
+import datetime
+import math
+
+''' Query date '''
+date = datetime.date.fromordinal(datetime.date.today().toordinal()-3)
+
+''' Output location '''
+fileName = "/opt/scripts/tasty/%s.txt" % (date)
+
+''' Taste Reports Query URL '''
+url = "http://tastereports.com/archives.html?type=added&stat_date=" + str(date) + "&registrar_id=all&Submit=Go&keyword="
+
+''' Regex '''
+regexDomain = r'\"domain\.html\?domain=([a-zA-Z0-9-.]{1,255})\"'
+regexNumOfDomains = r'\(([0-9,]{0,10})\sdomain'
+
+''' Request Headers '''
+headers = {
+        "Host" : "tastereports.com",
+	"User-Agent" : "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0",
+        "Connection" : "close",
+        "Upgrade-Insecure-Requests" : "1"
+}
+
+''' Used for debug '''
+'''
+	http_proxy = "http://127.0.0.1:8080"
+	https_proxy = "https://127.0.0.1:8080"
+	
+	proxyDict = {
+	        "http" : http_proxy,
+	        "https" : https_proxy
+	}
+
+	example:
+	r = requests.post(url, data="", headers=headers, proxies=proxyDict, verify=False)
+'''
+
+''' Perform HTTP requests '''
+def getDomains(pageNum):
+	newUrl = url + "&start=" + str(pageNum * 100)
+	req = requests.post(newUrl, data="", headers=headers, verify=False)
+	print "[+] Data retrieved from: %s, Status code: %s" % (newUrl, req.status_code) 
+	return req.text
+
+
+if __name__ == '__main__':
+	print "[!] Starting Tasty Downloader..."
+
+	''' Perform the web request '''
+	r = requests.post(url, data="", headers=headers, verify=False)
+
+	''' Get the number of domains '''
+	numOfDomains = re.findall(regexNumOfDomains, r.text)
+
+	''' Remove "," if there is a large number of domains '''
+	records = numOfDomains[0].replace(",", "")
+
+	print "[+] Found %s domains, starting download..." % (records)
+
+	''' Can only get 100 records at a time '''
+	webRequests =  math.ceil(float(records) / float(100))
+
+	''' Loop through all entries, than can be multithreaded later '''
+	for i in range(int(webRequests)):
+		data = getDomains(i)
+		domains = re.findall(regexDomain, data)
+
+		print "[+] Got: %s valid domains" % (len(domains))	
+
+		f = open(fileName, 'a')
+
+		''' Create / write domains to a text file '''
+		for domain in domains:
+			f.write(domain + "\n")
+		f.close()
