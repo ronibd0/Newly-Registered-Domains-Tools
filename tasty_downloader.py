f5642687d9c7a5774f1fd9dc77fde08f8263e3a4
root <root@ip-172-31-28-34.us-west-2.compute.internal>
2018-01-15 02:23:07 +0000

    Fixed caching issue

--- tasty_downloader.py
+++ tasty_downloader.py
@@ -23,7 +23,10 @@ headers = {
         "Host" : "tastereports.com",
 	"User-Agent" : "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0",
         "Connection" : "close",
-        "Upgrade-Insecure-Requests" : "1"
+        "Upgrade-Insecure-Requests" : "1",
+        "Cache-Control" : "no-cache, no-store, must-revalidate",
+        "Pragma" : "no-cache",
+        "Expires" : "0"
 }
 
 ''' Used for debug '''
